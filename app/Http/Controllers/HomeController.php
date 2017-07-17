964b43d4eeb39e493b0da93069f0d9a7e9a17626
dessurya <fourline66@gmail.com>
2017-07-17 13:28:45 +0700

    home view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\ProductSellPrice;
+
+use Auth;
+use DB;
+use Validator;
+
+class HomeController extends Controller{
+	public function index(){
+    	$ProductSellPrice = ProductSellPrice::get();
+    	
+    	return view('home.index', compact(
+			'ProductSellPrice'
+		));
+    }
+}
