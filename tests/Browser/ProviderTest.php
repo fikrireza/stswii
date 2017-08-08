203d4644f3826bd9759c7e4adab5febcc675ba8c
Jonathan Dharmawan <jonathan.digindo@gmail.com>
2017-08-08 16:28:37 +0700

    Add Scenario Delay

--- tests/Browser/ProviderTest.php
+++ tests/Browser/ProviderTest.php
@@ -19,8 +19,11 @@ class ProviderTest extends DuskTestCase
         $this->browse(function ($browser) {
             $browser->loginAs(User::find(3))
                     ->visit('/home')
+                    ->pause(2000)
                     ->clickLink('Manage Provider')
+                    ->pause(2000)
                     ->click('.menu_section>ul>li:nth-child(2)>ul>li:nth-child(1)>a')
+                    ->pause(2000)
                     ->assertPathIs('/stswii/public/provider');
         });
     }
@@ -30,9 +33,13 @@ class ProviderTest extends DuskTestCase
         $this->browse(function ($browser) {
             $browser->loginAs(User::find(3))
                     ->visit('/provider')
+                    ->pause(2000)
                     ->clickLink('Add')
+                    ->pause(2000)
                     ->type('input[name=provider_name]', 'Example Provider')
+                    ->pause(2000)
                     ->press('Submit')
+                    ->pause(2000)
                     ->assertSee('Example Provider');
         });
     }
@@ -42,7 +49,9 @@ class ProviderTest extends DuskTestCase
         $this->browse(function ($browser) {
             $browser->loginAs(User::find(3))
                     ->visit('/provider')
+                    ->pause(2000)
                     ->clickLink('Add')
+                    ->pause(2000)
                     ->press('Submit')
                     ->pause(2000)
                     ->assertSee('mohon isi');
@@ -54,8 +63,11 @@ class ProviderTest extends DuskTestCase
         $this->browse(function ($browser) {
             $browser->loginAs(User::find(3))
                     ->visit('/provider')
+                    ->pause(2000)
                     ->clickLink('Add')
+                    ->pause(2000)
                     ->type('input[name=provider_name]', 'Example Provider')
+                    ->pause(2000)
                     ->press('Submit')
                     ->pause(2000)
                     ->assertSee('Provider ini sudah ada');
@@ -69,9 +81,13 @@ class ProviderTest extends DuskTestCase
                     ->visit('/provider')
                     ->waitFor('#dataTables')
                     ->click('#dataTables>tbody>tr:nth-last-child(1)>td:nth-last-child(1)>a:nth-child(2)')
+                    ->pause(2000)
                     ->clear('#name_provider_update')
+                    ->pause(2000)
                     ->type('#name_provider_update', 'Example Provider Edited')
+                    ->pause(2000)
                     ->press('Submit')
+                    ->pause(2000)
                     ->assertSee('Example Provider Edited');
         });
     }
@@ -83,7 +99,9 @@ class ProviderTest extends DuskTestCase
                     ->visit('/provider')
                     ->waitFor('#dataTables')
                     ->click('#dataTables>tbody>tr:nth-last-child(1)>td:nth-last-child(1)>a:nth-child(3)')
+                    ->pause(2000)
                     ->click('#setDelete')
+                    ->pause(2000)
                     ->assertDontSee('Example Provider Edited');
         });
     }
