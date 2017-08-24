60499b39a221b609e92cb60a9e244d57e74bc826
fikrireza <fikrirezaa@gmail.com>
2017-08-24 16:23:10 +0700

    jegat tanggal start

--- resources/views/product-sell-price/ubah.blade.php
+++ resources/views/product-sell-price/ubah.blade.php
@@ -177,6 +177,7 @@
     "timePicker24Hour": true,
     "timePickerSeconds": true,
     "timePickerIncrement": 1,
+    "minDate": new Date(),
   });
 
   $('#datetime_end').daterangepicker({
@@ -188,6 +189,7 @@
     "timePicker24Hour": true,
     "timePickerSeconds": true,
     "timePickerIncrement": 1,
+    "minDate": new Date(),
   });
 
   $('#flg_tax').click(function() {
